76f7f32bdea6549133eeab63249e9dba7be4e7ae
nashera28bomu-pixel <nashera28bomu@gmail.com>
2026-06-10 06:23:50 +0300

    Delete routers directory

--- routers/recommendations.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from utils.recommendations import get_movie_recommendations
-
-# THIS IS THE MISSING PIECE
-router = APIRouter()
-
-@router.get("/{imdb_id}/recommendations")
-async def get_recommendations(imdb_id: str):
-    return await get_movie_recommendations(imdb_id)
